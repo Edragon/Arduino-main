e9581eb8b7ec21390fb4ee03acb260b4fbc24484
edragon <info@electodragon.com>
2017-11-20 00:35:28 +0800

    update

---
+++ Sketchbook/NGS/GPS_UART/GPS_UART.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+  Serial.begin(38400);
+}
+
+void loop()
+{
+  // make a string for assembling the data to log:
+  char index = 0;
+  char temp = 0;
+  String dataString = "";
+
+   while(Serial.available())
+   {
+     temp = Serial.read();
+     dataString += String(temp);
+     index++;
+     if(index>200)
+       break;
+   }
+   Serial.print(dataString);
+ }
