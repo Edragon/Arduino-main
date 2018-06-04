02025173c86951085c8133d766c72dd9e693109b
edragon <info@electrodragon.com>
2018-06-04 12:12:48 +0800

    update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  * Include all need libarires, pre-defined sketches
  * In arduino - preference - simply set "Project file location here"
  * to this folder, and use all libaries, sketchbook together.
+
+ ** PCF8574 has two libraries, better use PCF8574_2.h
--- Sketchbook/11-Interface/uart_debug/uart_debug.ino
+++ Sketchbook/11-Interface/uart_debug/uart_debug.ino
@@ -2,9 +2,9 @@
 SoftwareSerial serial_test(3, 4);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (!Serial);
-  serial_test.begin(115200);
+  serial_test.begin(9600);
   delay(1000);
   Serial.println("Setup Complete!");
   //serial_test.println("AT+RST");
