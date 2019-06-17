1d6d93de5eccd752be445865e22fd5756b18cdc2
edragon <info@electrodragon.com>
2019-06-17 12:23:08 +0800

    update

--- arduino-env/Sketchbook/01_GSM/sim868 module test final/sim868 module test final.ino
+++ arduino-env/Sketchbook/01_GSM/sim868 module test final/sim868 module test final.ino
@@ -1,29 +1,56 @@
 #include <SoftwareSerial.h>
 SoftwareSerial mySerial(2, 3);
-int pushButton = 5;
+int loop_flag = 5;
 int boot = 4;
 int REG_flag = 0;
 int GPS_flag = 0;
 int status_LED = 13;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
   mySerial.begin(115200);
 
   pinMode(boot, OUTPUT);
-  pinMode(pushButton, INPUT);
+  pinMode(loop_flag, OUTPUT);
   pinMode(status_LED, OUTPUT);
 
-  // boot it
-  digitalWrite(4, HIGH);    // turn on GPS
-  delay(2000);                       // wait for a second
-  //GPS();
-  Serial.println("init done ..");
-  delay(1000);
+  digitalWrite(loop_flag, HIGH);
+
+  GPS_init();
 }
 
+
+
 void loop() {
+  loop_select();
+
+}
+
+void loop_select() {
+  int buttonState = digitalRead(loop_flag);
+
+  if ( buttonState == 0 && REG_flag == 1 ) {
+    REG_flag = 2;
+    call();
+
+  }
+
+  if ( buttonState == 1 && GPS_flag == 0) {
+    GPS_flag = 1;
+    Serial.println("DO GPS");
+    GPS();
+  }
+  
+  if (GPS_flag == 1) {
+    mySerial.println("AT+CGNSINF\r\n");
+    delay(100);
+    ShowSerialData();
+    delay(1000);
+  }
+}
+
+void SIM_status () {
   mySerial.println("AT+CPIN?\r\n");
   if  (mySerial.find("READY") ) {
     Serial.println("Found READY!");
@@ -42,19 +69,25 @@ void loop() {
   }
   mySerial.flush();
   delay(1000);
+}
 
-  int buttonState = digitalRead(pushButton);
-  if ( buttonState == 0 && REG_flag == 1 ) {
-    REG_flag = 2;
-    call();
 
+void GPS_init() {
+  mySerial.println("AT?\r\n");
+  if (mySerial.find("OK") ) {
+    Serial.println("already boot  ..");
   }
-
-  if ( buttonState == 1 && GPS_flag == 0) {
-    GPS_flag = 1;
-    GPS();
+  else {
+    // boot it
+    digitalWrite(boot, HIGH);    // turn on GPS
+    delay(3000);
+    digitalWrite(boot, LOW);// wait for a second
+    //GPS();
+    Serial.println("restart done ..");
+    delay(1000);
   }
 }
+
 
 
 void blink_LED() {
@@ -65,15 +98,15 @@ void blink_LED() {
   digitalWrite(13, HIGH);
   delay(50);
   digitalWrite(13, LOW);
-  delay(50);  
+  delay(50);
   digitalWrite(13, HIGH);
   delay(50);
   digitalWrite(13, LOW);
-  delay(50);  
+  delay(50);
   digitalWrite(13, HIGH);
   delay(50);
   digitalWrite(13, LOW);
-  delay(50);  
+  delay(50);
   digitalWrite(13, HIGH);
   delay(50);
   digitalWrite(13, LOW);
@@ -107,7 +140,8 @@ void GPS() {
   delay(100);
   ShowSerialData();
   delay(1000);
-  Serial.println("turned on GPS ...");
+
+  Serial.println("GPS turned on  ...");
 }
 
 void ShowSerialData()
