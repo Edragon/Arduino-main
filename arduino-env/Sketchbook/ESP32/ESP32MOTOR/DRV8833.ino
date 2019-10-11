39af83e24723a8ac2f37e806849d373d2fa96369
edragon <info@electrodragon.com>
2019-10-11 18:18:48 +0800

    update

---
+++ arduino-env/Sketchbook/ESP32/ESP32MOTOR/DRV8833.ino
@@ -0,0 +1,47 @@
+
+const int IA1 = 16;
+const int IA2 = 17;
+const int IB1 = 18;
+const int IB2 = 19;
+
+int beeper = 5;
+
+void setup() {
+  //pinMode(IA1, OUTPUT);
+  pinMode(IA2, OUTPUT);
+  //pinMode(IB1, OUTPUT);
+  pinMode(IB2, OUTPUT);
+
+  ledcAttachPin(IA1, 1);
+  ledcAttachPin(IB1, 2);
+  ledcAttachPin(beeper, 3);
+
+  // Initialize channels
+  // channels 0-15, resolution 1-16 bits, freq limits depend on resolution
+  // ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
+  ledcSetup(1, 12000, 8); // 12 kHz PWM, 8-bit resolution
+  ledcSetup(2, 12000, 8);
+  ledcSetup(3, 500, 8);
+}
+
+
+
+
+void loop() {
+  digitalWrite(IA2, LOW);
+  digitalWrite(IB2, LOW);
+
+
+  ledcWrite(1, 255);
+  ledcWrite(2, 125);
+
+  ledcWrite(3, 125);
+  delay(5000);
+
+
+  ledcWrite(1, 0);
+  ledcWrite(2, 0);
+  
+  ledcWrite(3, 244);
+  delay(5000);
+}
---
+++ arduino-env/libraries/Adafruit_SHT31_Library/Adafruit_SHT31.cpp
@@ -0,0 +1,164 @@
+/*!
+ *  @file Adafruit_SHT31.cpp
+ *
+ *  @mainpage Adafruit SHT31 Digital Humidity & Temp Sensor
+ *
+ *  @section intro_sec Introduction
+ *
+ *  This is a library for the SHT31 Digital Humidity & Temp Sensor
+ *
+ *  Designed specifically to work with the SHT31 Digital sensor from Adafruit
+ *
+ *  Pick one up today in the adafruit shop!
+ *  ------> https://www.adafruit.com/product/2857
+ *
+ *  These sensors use I2C to communicate, 2 pins are required to interface
+ *
+ *  Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit andopen-source hardware by purchasing products
+ *  from Adafruit!
+ *
+ *  @section author Author
+ *
+ *  Limor Fried/Ladyada (Adafruit Industries).
+ *
+ *  @section license License
+ *
+ *  BSD license, all text above must be included in any redistribution
+ */
+
+#include "Adafruit_SHT31.h"
+
+/*!
+ * @brief  SHT31 constructor using i2c
+ * @param  *theWire
+ *         optional wire
+ */
+Adafruit_SHT31::Adafruit_SHT31(TwoWire *theWire) {
+  _wire = theWire;
+  _i2caddr = NULL;
+  humidity = 0.0f;
+  temp = 0.0f;
+}
+
+boolean Adafruit_SHT31::begin(uint8_t i2caddr) {
+  _wire->begin();
+  _i2caddr = i2caddr;
+  reset();
+  // None connected sensors seem to return decimal 65535.
+  return readStatus() != 65535;
+}
+
+uint16_t Adafruit_SHT31::readStatus(void) {
+  writeCommand(SHT31_READSTATUS);
+  _wire->requestFrom(_i2caddr, (uint8_t)3);
+  uint16_t stat = _wire->read();
+  stat <<= 8;
+  stat |= _wire->read();
+  // Serial.println(stat, HEX);
+  return stat;
+}
+
+void Adafruit_SHT31::reset(void) {
+  writeCommand(SHT31_SOFTRESET);
+  delay(10);
+}
+
+void Adafruit_SHT31::heater(boolean h) {
+  if (h)
+    writeCommand(SHT31_HEATEREN);
+  else
+    writeCommand(SHT31_HEATERDIS);
+}
+
+float Adafruit_SHT31::readTemperature(void) {
+  if (!readTempHum())
+    return NAN;
+
+  return temp;
+}
+
+float Adafruit_SHT31::readHumidity(void) {
+  if (!readTempHum())
+    return NAN;
+
+  return humidity;
+}
+
+boolean Adafruit_SHT31::readTempHum(void) {
+  uint8_t readbuffer[6];
+
+  writeCommand(SHT31_MEAS_HIGHREP);
+
+  delay(20);
+  _wire->requestFrom(_i2caddr, (uint8_t)6);
+  if (_wire->available() != 6)
+    return false;
+  for (uint8_t i = 0; i < 6; i++) {
+    readbuffer[i] = _wire->read();
+    //  Serial.print("0x"); Serial.println(readbuffer[i], HEX);
+  }
+
+  uint16_t ST, SRH;
+  ST = readbuffer[0];
+  ST <<= 8;
+  ST |= readbuffer[1];
+
+  if (readbuffer[2] != crc8(readbuffer, 2))
+    return false;
+
+  SRH = readbuffer[3];
+  SRH <<= 8;
+  SRH |= readbuffer[4];
+
+  if (readbuffer[5] != crc8(readbuffer + 3, 2))
+    return false;
+
+  // Serial.print("ST = "); Serial.println(ST);
+  double stemp = ST;
+  stemp *= 175;
+  stemp /= 0xffff;
+  stemp = -45 + stemp;
+  temp = stemp;
+
+  //  Serial.print("SRH = "); Serial.println(SRH);
+  double shum = SRH;
+  shum *= 100;
+  shum /= 0xFFFF;
+
+  humidity = shum;
+
+  return true;
+}
+
+void Adafruit_SHT31::writeCommand(uint16_t cmd) {
+  _wire->beginTransmission(_i2caddr);
+  _wire->write(cmd >> 8);
+  _wire->write(cmd & 0xFF);
+  _wire->endTransmission();
+}
+
+uint8_t Adafruit_SHT31::crc8(const uint8_t *data, int len) {
+  /*
+   *
+   * CRC-8 formula from page 14 of SHT spec pdf
+   *
+   * Test data 0xBE, 0xEF should yield 0x92
+   *
+   * Initialization data 0xFF
+   * Polynomial 0x31 (x8 + x5 +x4 +1)
+   * Final XOR 0x00
+   */
+
+  const uint8_t POLYNOMIAL(0x31);
+  uint8_t crc(0xFF);
+
+  for (int j = len; j; --j) {
+    crc ^= *data++;
+
+    for (int i = 8; i; --i) {
+      crc = (crc & 0x80) ? (crc << 1) ^ POLYNOMIAL : (crc << 1);
+    }
+  }
+  return crc;
+}
