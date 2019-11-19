e46887bb61cdea3439574eea2ba4400a638dad2d
edragon <info@electrodragon.com>
2019-11-19 18:35:28 +0800

    fix can

--- arduino-env/Sketchbook/03_ESP32/ESP32MOTOR/DRV8833/DRV8833.ino
+++ arduino-env/Sketchbook/03_ESP32/ESP32MOTOR/DRV8833/DRV8833.ino
@@ -21,27 +21,28 @@ void setup() {
   // ledcSetup(uint8_t channel, uint32_t freq, uint8_t resolution_bits);
   ledcSetup(1, 12000, 8); // 12 kHz PWM, 8-bit resolution
   ledcSetup(2, 12000, 8);
-  ledcSetup(3, 500, 8);
+  ledcSetup(3, 800, 8);  // channel 3, 500khz, 8bit
 }
 
 
 
 
 void loop() {
+  //ledcWrite(3, 0);
   digitalWrite(IA2, LOW);
   digitalWrite(IB2, LOW);
 
 
-  ledcWrite(1, 255);
+  ledcWrite(1, 255); // motor 1 and 2
   ledcWrite(2, 125);
 
-  ledcWrite(3, 125);
+  ledcWrite(3, 0); // buzzer
   delay(5000);
 
 
-  ledcWrite(1, 0);
+  ledcWrite(1, 0);  // motor 1 and 2
   ledcWrite(2, 0);
   
-  ledcWrite(3, 244);
+  ledcWrite(3, 125); // duty 125
   delay(5000);
 }
--- arduino-env/Sketchbook/18-protocol/can-send-2/can-send-2.ino
+++ arduino-env/Sketchbook/18-protocol/can-send-2/can-send-2.ino
@@ -2,16 +2,18 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
+
+
 void setup()
 {
   Serial.begin(115200);
-  // init can bus, baudrate: 500k
-  if (CAN.begin(CAN_500KBPS) == CAN_OK) Serial.print("can init ok!!\r\n");
+  // init can bus, baudrate: 100k
+  if (CAN.begin(CAN_100KBPS) == CAN_OK) Serial.print("can init ok!!\r\n");
   else Serial.print("Can init fail!!\r\n");
 }
 
 unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
-unsigned char stmp2[8] = {0, 1, 2, 3, 4, 5, 6, 9};
+unsigned char stmp2[8] = {8, 7, 6, 5, 4, 3, 2, 1};
 void loop()
 {
   // send data:  id = 0x00, standrad flame, data len = 8, stmp: data buf
