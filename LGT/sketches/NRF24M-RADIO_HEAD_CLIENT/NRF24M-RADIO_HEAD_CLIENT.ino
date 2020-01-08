80ec1e39efcf09719146187f53f8a31eba7ea78a
hechao <info@electrodragon.com>
2020-01-08 14:07:20 +0800

    update

---
+++ LGT/sketches/NRF24M-RADIO_HEAD_CLIENT/NRF24M-RADIO_HEAD_CLIENT.ino
@@ -0,0 +1,79 @@
+// nrf24_client.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messageing client
+// with the RH_NRF24 class. RH_NRF24 class does not provide for addressing or
+// reliability, so you should only use RH_NRF24 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example nrf24_server.
+// Tested on Uno with Sparkfun NRF25L01 module
+// Tested on Anarduino Mini (http://www.anarduino.com/mini/) with RFM73 module
+// Tested on Arduino Mega with Sparkfun WRL-00691 NRF25L01 module
+
+#include <SPI.h>
+#include <RH_NRF24.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(5, 6); // RX, TX
+
+// Singleton instance of the radio driver
+
+// CE 16 CS 15
+RH_NRF24 nrf24(9, 7);
+// RH_NRF24 nrf24(8, 7); // use this to be electrically compatible with Mirf
+// RH_NRF24 nrf24(8, 10);// For Leonardo, need explicit SS pin
+// RH_NRF24 nrf24(8, 7); // For RFM73 on Anarduino Mini
+
+void setup() 
+{
+  pinMode(3, OUTPUT);
+  //Serial.begin(9600);
+  mySerial.begin(9600);
+
+  if (!nrf24.init())
+    mySerial.println("init failed");
+  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!nrf24.setChannel(1))
+    mySerial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    mySerial.println("setRF failed");    
+  //mySerial.println("init OK\r\n");  
+   
+}
+
+
+void loop()
+{
+  digitalWrite(3, HIGH);  
+  mySerial.println("Sending to nrf24_server");
+  // Send a message to nrf24_server
+  uint8_t data[] = "Hello World!";
+  nrf24.send(data, sizeof(data));
+  
+  nrf24.waitPacketSent();
+  // Now wait for a reply
+  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  
+  if (nrf24.waitAvailableTimeout(500))
+  { 
+      
+    // Should be a reply message for us now   
+    if (nrf24.recv(buf, &len))
+    {
+      digitalWrite(3, LOW);
+      mySerial.print("got reply: ");
+      mySerial.println((char*)buf);
+    }
+    else
+    {
+      digitalWrite(3, LOW);
+      mySerial.println("recv failed");
+    }
+  }
+  else
+  {
+    digitalWrite(3, LOW);
+    mySerial.println("No reply, is nrf24_server running?");
+  }
+  delay(400);
+  
+}
