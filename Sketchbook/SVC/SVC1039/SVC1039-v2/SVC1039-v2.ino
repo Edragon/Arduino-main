f7a6a94f6460dc38c91a1f935d2251ce0f60bc9b
hechaogm <hechaogm@gmail.com>
2022-12-29 19:06:59 +0800

    update

--- Sketchbook/SVC/SVC1039/SVC1039-v2/SVC1039-v2.ino
+++ Sketchbook/SVC/SVC1039/SVC1039-v2/SVC1039-v2.ino
@@ -13,19 +13,24 @@
 
 SSD1306AsciiWire oled;
 
-//int flag = 1;
 
+int flag = 1;
+  
 void setup() {
+
   Wire.begin();
+  
   Wire.setClock(400000L);
 
   oled.begin(&Adafruit128x64, 0x3C);
 
   oled.setFont(Adafruit5x7);
 
-  //pinMode(relay, OUTPUT);
-  //pinMode(led, OUTPUT);
+  pinMode(relay, OUTPUT);
+  pinMode(led, OUTPUT);
+  
 }
+
 
 void loop() {
 
@@ -49,15 +54,15 @@ void loop() {
   oled.print("buttons: ");
   oled.println(bt);
 
-//  if (flag = 1) {
-//    flag = 0;
-//    digitalWrite(relay, HIGH);
-//    digitalWrite(led, HIGH);
-//  } else {
-//    flag = 1;
-//    digitalWrite(relay, LOW);
-//    digitalWrite(led, LOW);
-//  }
+  if (flag == 1) {
+    flag = 0;
+    digitalWrite(relay, HIGH);
+    digitalWrite(led, HIGH);
+  } else {
+    flag = 1;
+    digitalWrite(relay, LOW);
+    digitalWrite(led, LOW);
+  }
   delay(1000);
 
 }
